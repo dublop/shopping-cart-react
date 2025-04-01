7de74ebdf5a8b8c0596755ce5f520124c1f13a80
dublop <dnlopezavila@gmail.com>
2025-04-01 14:54:20 -0400

    Add styles to navbar-cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { CartIcon } from './Icons'
 import { useSelector } from 'react-redux'
 
 const Navbar = () => {
   const [modal, setModal] = useState(true)
   const cart = useSelector((state) => state.cart.value)
+  const navigate = useNavigate()
 
   const handleModal = () => {
     setModal(prevState => !prevState)
@@ -19,8 +20,16 @@ const Navbar = () => {
         </ul>
         <button onClick={handleModal} className='bg-neutral-950 p-2 rounded-md hover:cursor-pointer'><CartIcon className="text-white" /></button>
         
-        <div hidden={modal} className='bg-neutral-950 text-white w-[300px] absolute top-16 right-0'>
-          <div>{cart.map((item)=> (<p key={item.id}>{item.title}</p>))}</div>
+        <div hidden={modal} className='flex flex-col gap-1.5 bg-neutral-950 text-white w-[300px] absolute top-16 right-0 p-[20px] rounded-md'>
+          {cart.map((item)=> (
+            <div className='w-[260px] flex flex-row items-center gap-1 p-[10px] border-1 border-neutral-400 rounded-md' key={item.id}>
+              <div className='w-[50px] h-[50px]'>
+                <img src={item.image} alt={item.category} className='w-full h-full object-contain' />
+              </div>
+              <p className='truncate'>{item.title}</p>
+              </div>
+            ))}
+            <button onClick={() => navigate('/cart')} className='w-full mt-2 p-2 bg-white text-neutral-950 rounded-md hover:cursor-pointer'>View cart</button>
         </div>
     </nav>
   )
